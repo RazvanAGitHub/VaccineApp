aea93cf40ad82d9eba78c7c23b49e8b2c5512996
DianaCT <diana.camelia.tapu@gmail.com>
2021-02-01 19:50:38 +0200

    Correcting sql data file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@ CREATE DATABASE covid19
  WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
+    --LC_COLLATE = 'English_United States.1252'
+    --LC_CTYPE = 'English_United States.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
@@ -53,19 +53,19 @@ CREATE TABLE public."user" (
 --
 
 INSERT INTO "user" VALUES 
-	(default, 'Albert Einstein','1790314011844', 'Princeton', 'albert.einstein@yahoo.com', '0766562789', '1', '2021-01-19 10:38:15', 1, default),
-  (default, 'Max Planck','1580423011834', 'Göttingen', 'max.planck@yahoo.com', '0744321987', '3', '2021-01-15 03:22:45', 2, default),
-  (default, 'Werner Heisenberg','1760201011869', 'Würzburg', 'werner.heisenberg@yahoo.com', '0769562871', '2', '2021-01-18 15:14:59', 1, default),
-  (default, 'Niels Bohr','1851007011831', 'Würzburg', 'niels.bohr@yahoo.com', '0766354268', '4', '2021-01-17 12:47:25', 3, default),
-  (default, 'Erwin Rudolf Josef Alexander Schrödinger','1870812011842', 'Vienna', 'erwin.schrödinger@yahoo.com', '0763989756', '1', '2021-01-11 07:35:33', 1, default);
+	(default, 'Albert Einstein','1790314011844', 'Princeton', 'albert.einstein@yahoo.com', '0766562789','Doctor', '1', '2021-01-19 10:38:15', 1, default),
+  (default, 'Max Planck','1580423011834', 'Göttingen', 'max.planck@yahoo.com', '0744321987','Other', '3', '2021-01-15 03:22:45', 2, default),
+  (default, 'Werner Heisenberg','1760201011869', 'Würzburg', 'werner.heisenberg@yahoo.com', '0769562871','Teacher', '2', '2021-01-18 15:14:59', 1, default),
+  (default, 'Niels Bohr','1851007011831', 'Würzburg', 'niels.bohr@yahoo.com', '0766354268','Doctor', '4', '2021-01-17 12:47:25', 3, default),
+  (default, 'Erwin Rudolf Josef Alexander Schrödinger','1870812011842', 'Vienna', 'erwin.schrödinger@yahoo.com', '0763989756','Other', '1', '2021-01-11 07:35:33', 1, default);
 
 
 CREATE DATABASE covid19security
 WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
+   -- LC_COLLATE = 'English_United States.1252'
+   -- LC_CTYPE = 'English_United States.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
